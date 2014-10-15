17df6facd4afcf16a7000945ff2162f1b9c25f4b
CleezyITP <ckv214@nyu.edu>
2014-10-15 13:46:25 -0400

    hi there

---
+++ react.js
@@ -0,0 +1,50 @@
+
+
+$( document ).ready(function() {
+
+$('.btn1').click(function() { 
+    alert("Correct! You should always seek a qualified lawyer with experience in immigration law."); 
+});
+
+$('.btn2').click(function() { 
+    alert("Wrong Answer! You should always seek a qualified lawyer with experience in immigration law."); 
+});
+
+$('.btn3').click(function() { 
+    alert("Correct! A lawyer that provides you with facts and figures about your case is being realistic."); 
+});
+
+$('.btn4').click(function() { 
+    alert("Wrong Answer! A lawyer that promises you 100% success in your application may be misleading you. "); 
+});
+
+$('.btn5').click(function() { 
+    alert("Correct! United States Citizen and Immigration Services are free. At most, you might be charged a small fee to help you to understand the forms. This doesnt have to be done thru a legal professional. Order forms by calling 1-800-870-3676"); 
+});
+
+$('.btn6').click(function() { 
+    alert("Wrong Answer! United States Citizen and Immigration Services are free. At most, you might be charged a small fee to help you to understand the forms. This doesnt have to be done thru a legal professional. Order forms by calling 1-800-870-3676"); 
+});
+
+console.log("ready!");
+
+});
+
+
+
+// });
+// });
+
+// $('.btn2').click(alert("Wrong Answer!"));
+
+
+// set the initial positions
+
+
+// after 660ms...
+// setTimeout(function() {
+//   // ...move the ball
+//   $('.fa-futbol-o').animate({
+//     left: 0
+//   }, 800, 'easeOutQuart');
+// }, 660);
